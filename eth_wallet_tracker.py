1846825eed3d64d76bf735b7454e40e5c0fa0acf
Anastasov <anastasovdavid@gmail.com>
2022-06-10 19:31:16 +0200

    finished

--- eth_wallet_tracker.py
+++ eth_wallet_tracker.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 from datetime import datetime
 
 API_KEY = "HD8T9DFW919M41VM3GW7UJSKT25X91P2T3"
-address = "0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae"
+address = "0x73bceb1cd57c711feac4224d062b0f6ff338501e"
 BASE_URL = "https://api.etherscan.io/api"
 ETHER_VALUE = 10 ** 18
 
@@ -15,29 +15,52 @@ def make_api_url(module, action, address, **kwargs):
     return url
 
 def get_account_balance(address):
-    get_balance_url = make_api_url("account","balance", address, tag="latest")
-    response = get(get_balance_url)
+    balance_url = make_api_url("account","balance", address, tag="latest")
+    response = get(balance_url)
     data = response.json()
     value = int(data["result"]) / ETHER_VALUE
     return value
 
 def get_transactions(address):
-    get_transactions_url = make_api_url("account", "txlist", address, startblock=0, endblock=99999999, page=1, offset=10000, sort="asc")
-    response = get(get_transactions_url)
+    transactions_url = make_api_url("account", "txlist", address, startblock=0, endblock=99999999, page=1, offset=10000, sort="asc")
+    response = get(transactions_url)
     data = response.json()["result"]
+
+    internal_tx_url = make_api_url("account", "txlistinternal", address, startblock=0, endblock=99999999, page=1, offset=10000, sort="asc")
+    response2 = get(internal_tx_url)
+    data2 = response2.json()["result"]
+
+    data.extend(data2)
+    data.sort(key=lambda x: int(x['timeStamp']))
+
+    current_balance = 0
+    balances = []
+    times = []
 
     for tx in data:
         to = tx["to"]
         from_address = tx["from"]
         value = int(tx["value"]) / ETHER_VALUE
-        gas = int(tx["gasUsed"]) * int(tx["gasPrice"]) / ETHER_VALUE
+
+        if "gasPrice" in tx:
+            gas = int(tx["gasUsed"]) * int(tx["gasPrice"]) / ETHER_VALUE
+        else:
+            gas = int(tx["gasUsed"]) / ETHER_VALUE
+
         time = datetime.fromtimestamp(int(tx["timeStamp"]))
-        print("--------------------")
-        print("To: ", to)
-        print("From: ", from_address)
-        print("Value: ", value)
-        print("Gas Cost: ", gas)
-        print("Time: ", time)
+        money_in = to.lower() == address.lower()
+
+        if money_in:
+            current_balance += value
+        else:
+            current_balance -= value + gas
+
+        balances.append(current_balance)
+        times.append(time)
+
+    print(current_balance)
+    plt.plot(times,balances)
+    plt.show()
 
 eth = get_account_balance(address)
 print(eth)
